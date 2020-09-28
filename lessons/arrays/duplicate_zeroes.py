9b166f97564e56c62dd106abec67c9b071c4004c
Andrew Dong <andrewdong1994@gmail.com>
2020-09-28 08:54:59 -0400

    Finish inserting into an array

---
+++ lessons/arrays/duplicate_zeroes.py
@@ -0,0 +1,36 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        possible_dups = 0
+        length_ = len(arr) - 1
+
+        # find the number of zeroes to be duplicated
+        for left in range(length_ + 1):
+            # Stop when left points beyond the last element in the original list
+            # which would be part of the modified list
+            if left > length_ - possible_dups:
+                break
+
+            # Count the zeros
+            if arr[left] == 0:
+                # Edge case: This zero can't be duplicated.  We have no more space
+                # Left is pointing to the last element which could be included
+                if left == length_ - possible_dups:
+                    arr[length_] = 0
+                    length_ -= 1
+                    break
+                possible_dups += 1
+
+        # Start backwards from the last element which would be part of new list
+        last = length_ - possible_dups
+
+        # Copy zero twice, and non zero once
+        for i in range(last, -1, -1):
+            if arr[i] == 0:
+                arr[i + possible_dups] = 0
+                possible_dups -= 1
+                arr[i + possible_dups] = 0
+            else:
+                arr[i + possible_dups] = arr[i]
